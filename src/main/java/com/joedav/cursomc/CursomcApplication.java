f5791802112ad7e71ffc75ab7c2abfa6278ba76a
Joelyson David <joelyson.davi@gmail.com>
2019-06-25 17:14:01 -0300

    Criando e relacionando estados e cidade

--- src/main/java/com/joedav/cursomc/CursomcApplication.java
+++ src/main/java/com/joedav/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.joedav.cursomc;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,33 +8,44 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.joedav.cursomc.domain.Categoria;
+import com.joedav.cursomc.domain.Cidade;
+import com.joedav.cursomc.domain.Estado;
 import com.joedav.cursomc.domain.Produto;
 import com.joedav.cursomc.repositories.CategoriaRepository;
+import com.joedav.cursomc.repositories.CidadeRepository;
+import com.joedav.cursomc.repositories.EstadoRepository;
 import com.joedav.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{ // implementa este Command para implementar um método par aexecutar um método ao startar
-	
+public class CursomcApplication implements CommandLineRunner { // implementa este Command para implementar um método par
+																// aexecutar um método ao startar
+
 	@Autowired // para ser instanciado automaticamente
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// instancia dois objetos do tipo categoria para serem implementados ao iniciar o projeto
-		Categoria cat1 = new Categoria(null, "Informática");	
+		// instancia dois objetos do tipo categoria para serem implementados ao iniciar
+		// o projeto
+		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		// instancia objeto do tipo produto
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
+
 		// associando categoria com os produtos
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -43,11 +53,27 @@ public class CursomcApplication implements CommandLineRunner{ // implementa este
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		// este método é responsavel para salvar os objetos que forem mandados (cat1 e cat2)
+
+		// este método é responsavel para salvar os objetos que forem mandados (cat1 e
+		// cat2)
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		// salvando os produtos
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		// instanciando Estados
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		// instanciando estados e cidades
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		// associando estado com as cidades
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		// salvando os dados no banco
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
